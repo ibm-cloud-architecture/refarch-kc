83f30a41c3630012e9af4b840b0a2966a497beb8
David Jones <djones6@uk.ibm.com>
2020-03-13 15:41:18 +0000

    Derive topic names from kafka-topics ConfigMap to remove duplication

--- scripts/localk8s/topics.yaml.sh
+++ scripts/localk8s/topics.yaml.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 SCRIPTLOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-TOPICS="bluewater-container
-bluewater-ship
-bluewater-problem
-orders
-order-commands
-rejected-orders
-allocated-orders
-errors
-containers
-reefer-telemetry
-container-anomaly-retry
-container-anomaly-dead"
+# Extract list of topics from kafka-topics ConfigMap resource (assumes
+# keys end with the string 'Topic')
+TOPICS=`cat $SCRIPTLOC/kafka-topics-configmap.yaml | grep 'Topic:' | cut -d':' -f2`
 
 # Note: topics must also be valid k8s resource names, otherwise they will
 # need to be converted (eg. lowercased) and specified via the 'topicName'
