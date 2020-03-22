ea022b975f49c56b458e8cbe5b2dd5cfa90cf9f1
Darnele Adhemar <darnelepierrelouis@gmail.com>
2020-03-22 18:20:37 -0400

    moved docs to carbon theme

---
+++ docs-gatsby/src/gatsby-theme-carbon/components/LeftNav/ResourceLinks.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ResourceLinks from 'gatsby-theme-carbon/src/components/LeftNav/ResourceLinks';
+
+const links = [
+  {
+    title: 'Github',
+    href: 'https://github.com/carbon-design-system/gatsby-theme-carbon',
+  },
+];
+
+// shouldOpenNewTabs: true if outbound links should open in a new tab
+const CustomResources = () => <ResourceLinks shouldOpenNewTabs links={links} />;
+
+export default CustomResources;
