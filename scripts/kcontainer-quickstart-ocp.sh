434316a2156faad8e1126fe9e2a84fcd38b0f12b
Jesús Almaraz Hernández <jesusalmarazhernandez@jesuss-mbp.home>
2021-02-09 18:41:06 +0100

    adding --skip-login flag for openLabs

--- scripts/kcontainer-quickstart-ocp.sh
+++ scripts/kcontainer-quickstart-ocp.sh
@@ -48,9 +48,38 @@ EOF
 ### MAIN ###
 ############
 
-oc login -u ${OCP_ADMIN_USER} -p ${OCP_ADMIN_PASSWORD}
+### Login
+# Make sure we don't have more than 1 argument
+if [[ $# -gt 1 ]];then
+ echo "Usage: sh kcontainer-quickstart-ocp.sh [--skip-login]"
+ exit 1
+fi
 
-##TODO## Add error check to validate login
+# Check the argument is what we expect
+if [[ $# -eq 1 ]];then
+  if [[ "$1" == "--skip-login" ]]; then
+    echo "Checking if you are logged into OpenShift..."
+    oc whoami
+    if [[ $? -gt 0 ]]; then
+      echo "[ERROR] - An error occurred while checking if you are logged into OpenShift"
+      exit 1
+    fi
+    echo "OK"
+    SKIP_LOGIN="true"
+  else
+    echo "Usage: sh kcontainer-quickstart-ocp.sh [--skip-login]"
+    exit 1
+  fi
+fi
+
+# Log in if we need to
+if [ -z $SKIP_LOGIN ]; then
+  oc login -u ${OCP_ADMIN_USER} -p ${OCP_ADMIN_PASSWORD}
+  if [[ $? -gt 0 ]]; then
+    echo "[ERROR] - An error occurred while logging into OpenShift"
+    exit 1
+  fi
+fi
 
 git clone https://github.com/ibm-cloud-architecture/refarch-kc-gitops.git
 
